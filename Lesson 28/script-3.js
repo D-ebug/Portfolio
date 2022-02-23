8e723ccf6d4542bf4d5e52d7ab355341381726ef
pc6 <52381178+D-ebug@users.noreply.github.com>
2022-02-23 17:52:17 +0200

    lesson 28

---
+++ Lesson 28/script-3.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('.inp');
+const create = document.querySelector('.create-collection');
+let numba = 0;
+create.addEventListener('click', () => {
+    let collection = document.createElement('ul');
+    newElem(inp.value);
+    if (num >= numba) {
+        collection.innerHTML = '';
+    }
+});
